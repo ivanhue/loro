cf6ad4270a352b1287e43b0c392fe06cf95c9615
Alejandro Villegas <alejandrovillegas.hu@gmail.com>
2026-03-24 13:15:52 -0600

    feat: parse .desktop files and return with App struct

---
+++ include/core/apps.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define MAX_APPS 512
+
+typedef struct {
+    char name[128];
+    char exec[256];
+    char icon[256];
+    char type[32];
+    int no_display;
+    int hidden;
+} App;
+
+int apps_load(App *out, int max);
